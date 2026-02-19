12d7bbcd64bb3154b2ba9751e7ce04ce0a5635b2
DUTTARGI-PAVAN <pavanduttargi10m.05@gmail.com>
2026-02-19 14:20:12 +0530

    done  database

--- Book/src/main/java/com/BookList/Book/BookRepo.java
+++ Book/src/main/java/com/BookList/Book/BookRepo.java
@@ -1,10 +1,8 @@
 package com.BookList.Book;
 
-import java.util.*;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepo extends JpaRepository<Book, Integer> {
     
-public Book getBookById(int id);
-public Book addBook(Book book);
 }
