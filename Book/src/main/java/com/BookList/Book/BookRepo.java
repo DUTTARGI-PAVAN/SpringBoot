2d9606634640665d28cc27d8b3087d0c5f5c3740
DUTTARGI-PAVAN <pavanduttargi10m.05@gmail.com>
2026-02-23 23:38:38 +0530

    done

--- Book/src/main/java/com/BookList/Book/BookRepo.java
+++ Book/src/main/java/com/BookList/Book/BookRepo.java
@@ -6,3 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookRepo extends JpaRepository<Book, Integer> {
     
 }
+
+public interface BookRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
